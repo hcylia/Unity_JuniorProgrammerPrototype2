daf8aa3524c37567697e47c44e486c7ba89023b5
Cylia <hcylia@outlook.fr>
2021-05-30 14:03:50 +0200

    Prefab and destroy

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float xRange = 30;
+    void Update()
+    {
+        if (transform.position.z > xRange || transform.position.z < -xRange)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
-    
+    public GameObject projectilePrefab;
     void Update()
     {
         if (transform.position.x < -xRange)
@@ -18,6 +18,11 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch Projectile
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
         
     }
 }
