f51b9279b00666295d7c45709c72679938e0f412
Cylia <hcylia@outlook.fr>
2021-05-31 20:10:42 +0200

    Challenge 2 - Dog

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private int nbFrame = 0;
+    private int maxFrame = 15;
+    // Update is called once per frame
+    void Update()
+    {
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && nbFrame > maxFrame)
+        {
+            nbFrame = 0;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+        nbFrame++;
+    }
+}
---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnLowRange = 3;
+    private float spawnTopRange = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomBall", startDelay, SpawnInterval());
+    }
+
+    float SpawnInterval()
+    {
+        return Random.Range(spawnLowRange, spawnTopRange);
+    }
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall()
+    {
+        
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+    }
+
+}
