0cf7105c5c909b97c9afb1b41c671bab34b1961e
Cylia <hcylia@outlook.fr>
2021-06-03 19:31:31 +0200

    Player moves freely on vertical and horizontal axis

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,22 +2,36 @@
 
 public class PlayerController : MonoBehaviour
 {
-    public float horizontalInput;
-    public float speed = 10.0f;
-    public float xRange = 10.0f;
     public GameObject projectilePrefab;
+
+    private float horizontalInput;
+    private float verticalInput;
+    private float speed = 10.0f;
+    private float xRange = 15.0f;
+    private float[] yRange = { 0.0f, 15.0f };
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
         if (transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.y);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
         if (transform.position.x > xRange)
         {
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.y);
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        if (transform.position.z < yRange[0])
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, yRange[0]);
+        }
+        if (transform.position.z > yRange[1])
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, yRange[1]);
+        }
+        
         if (Input.GetKeyDown(KeyCode.Space))
         {
             // Launch Projectile
