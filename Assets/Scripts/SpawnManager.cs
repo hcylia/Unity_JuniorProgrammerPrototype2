97e62d1b1d388211b83b8477f218e2512d7a3dd4
Cylia <hcylia@outlook.fr>
2021-06-03 19:48:18 +0200

    First try of left right spawning not optimal

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,6 +5,8 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 15;
     private float spawnPosZ = 20;
+    private float[] spawnRangeY = { 0.0f, 15.0f };
+    private float spawnPosX = 25;
     private float startDelay = 2;
     private float spawnInterval = 1.5f;
 
@@ -15,7 +17,20 @@ public class SpawnManager : MonoBehaviour
     void SpawnRandomAnimal()
     {
         int animalIndex = Random.Range(0, animalPrefabs.Length);
-        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        int spawnSide = Random.Range(0, 3);
+        Vector3 spawnPos;
+        if (spawnSide < 1)
+        {
+            spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        }
+        else
+        {
+
+            spawnPos = new Vector3( Mathf.Pow(-1, spawnSide) * spawnPosX, 0, Random.Range(spawnRangeY[0], spawnRangeY[1]));
+        }
+            
+
         Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
